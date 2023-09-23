94cb03d2cb5c53d5af12747d2e529aa87da35df1
Sujay-code750 <sujaykarmokar86@gmail.com>
2023-09-23 12:42:09 +0600

    first commit

---
+++ bootstrap.js
@@ -0,0 +1,8 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer.", "Designer.", "Blogger.",  "Freelancer."],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true,
+        currentSpeed:50,
+    });
